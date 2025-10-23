98505f80da8cce92eeb3747818e512fc78f8fe07
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-10-23 13:32:11 +0300

    Add getters

--- inc/matrix.hpp
+++ inc/matrix.hpp
@@ -13,13 +13,13 @@ class Matrix {
     requires std::same_as<std::iter_value_t<Iter>, T>
     
     explicit Matrix(const std::size_t n): 
-        rows_(n), cols_(n), JaggedArray(n, n) {}
+        n_rows_(n), n_cols(n), JaggedArray(n, n) {}
 
-    Matrix(const std::size_t rows, const std::size_t cols): 
-        rows_(rows), cols_(cols), JaggedArray(rows, cols) {}
+    Matrix(const std::size_t n_rows, const std::size_t n_cols): 
+        n_rows_(n_rows), n_cols_(n_cols), JaggedArray(n_rows, n_cols) {}
 
-    Matrix(const std::size_t rows, const std::size_t cols, Iter elems_begin, Iter elems_end): 
-        rows_(rows), cols_(cols), JaggedArray(rows, cols, elems_begin, elems_end) {}
+    Matrix(const std::size_t n_rows_, const std::size_t n_cols, Iter elems_begin, Iter elems_end): 
+        n_rows_(n_rows), n_cols(n_cols), JaggedArray(n_rows, n_cols, elems_begin, elems_end) {}
 
     static Matrix<T> diag(const std::size_t n, const T value) {
         Matrix<T> eye_matrix(n, n);
@@ -29,23 +29,23 @@ class Matrix {
         return eye_matrix;
     }
 
-    static Matrix<T> eye(const std::size_t n) {
+    static Matrix<T> identity(const std::size_t n) {
         return diag(n, T(1.0));
     }
 
   public: // Operators
-    Array<T> &operator[](const std::size_t idx) {
+    Array<T>& operator[](const std::size_t idx) {
         return data_[idx];
     }
 
-    const Array<T> &operator[](const std::size_t idx) const {
+    const Array<T>& operator[](const std::size_t idx) const {
         return data_[idx];
     }
 
   public: // Math
-    Matrix<T>& negate() & {
-        for (std::size_t i = 0; i < rows_; i++) {
-            for (std::size_t j = 0; j < cols_; j++) {
+    Matrix<T>& negate()& {
+        for (std::size_t i = 0; i < n_rows_; i++) {
+            for (std::size_t j = 0; j < n_cols_; j++) {
                 data_[i][j] *= T(-1.0);
             }
         }
@@ -53,8 +53,8 @@ class Matrix {
         return *this;
     }
 
-    Matrix& transpose() & {
-        for (std::size_t i = 0; i < rows_; i++) {
+    Matrix& transpose()& {
+        for (std::size_t i = 0; i < n_rows_; i++) {
             for (std::size_t j = 0; j < i; j++) {
                 std::swap(data_[i][j], data_[j][i]);
             }
@@ -76,16 +76,20 @@ class Matrix {
     }
 
     void add_row_to_row(const std::size_t dst, const std::size_t src, const T mul=T(1.0)) {
-        for (std::size_t i = 0; i < cols_; i++) {
+        for (std::size_t i = 0; i < n_cols_; i++) {
             data_[dst][i] += data_[src][i] * mul;
         }
     }
 
     T determinant() const;
 
+  public: // Getters
+    std::size_t n_rows() const { return n_rows_; }
+    std::size_t n_cols() const { return n_cols_; } 
+
   private:
-    std::size_t rows_ = 0
-    std::size_t cols_ = 0;
+    std::size_t n_rows_ = 0
+    std::size_t n_cols_ = 0;
     JaggedArray<T> data_;
 }
 
