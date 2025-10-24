a562c91c642cb159477759aa693acd384515a5c4
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-10-24 20:06:31 +0300

    add eigen compare

--- main.cpp
+++ main.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
-#include <vector>
 
 #include <matrix.hpp>
+#include <vector>
+#include <Eigen/Dense>
 
-int main () {
-    std::size_t size = 0;
-    std::cin >> size;
 
-    mtx::Matrix<long double> matrix(size);
-    for (std::size_t i = 0; i < size; i++) {
-        for (std::size_t j = 0; j < size; j++) {
-            std::cin >> matrix[i][j];
+void scan_matrix(const std::size_t n, std::vector<std::vector<double>> &matrix) {
+    for (std::size_t i = 0; i < n; i++) {
+        for (std::size_t j = 0; j < n; j++) {
+            std::cin >> matrix[i][i];
+        }
+    }
+}
+
+double get_eigen_determinant(const std::size_t n, const std::vector<std::vector<double>> &input_matrix) {
+    Eigen::MatrixXd A(n, n); 
+
+    for (std::size_t i = 0; i < n; i++) {
+        for (std::size_t j = 0; j < n; j++) {
+            A(i, j) = input_matrix[i][j];
         }
     }
 
-    std::cout << matrix.determinant();
+    return A.determinant();
 }
+
+double get_our_determinant(const std::size_t n, const std::vector<std::vector<double>> &input_matrix) {
+    mtx::Matrix<double> A(n, n);
+    for (std::size_t i = 0; i < n; i++) {
+        for (std::size_t j = 0; j < n; j++) {
+            A[i][j] = input_matrix[i][j];
+        }
+    }
+
+    return A.determinant();
+}
+
+
+int main () {
+    std::size_t n = 0;
+    std::cin >> n;
+
+    std::vector<std::vector<double>> matrix(n, std::vector<double>(n));
+    scan_matrix(n, matrix);
+
+    std::cout << "eigen : " << get_eigen_determinant(n, matrix) << '\n';
+    std::cout << "out   : " << get_our_determinant(n, matrix) << '\n';
+}
+
+
+
+
