4ce17a1122fdaa8fdacb3211980cb9911b09f5f5
Aleksandr <baas157@bk.ru>
2025-10-25 17:14:23 +0300

    project build fixed

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main () {
     std::size_t size = 0;
     std::cin >> size;
 
-    mtx::Matrix<long double> matrix(size);
+    mtx::Matrix<double> matrix(size);
     for (std::size_t i = 0; i < size; i++) {
         for (std::size_t j = 0; j < size; j++) {
             std::cin >> matrix[i][j];
