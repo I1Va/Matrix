dd1d6b237775dbd2eed9f2f5ae693aa8af4fbe14
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-11-06 00:16:43 +0300

    cin incorect symbols processing added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <istream>
+#include <sstream>
 #include <iostream>
 #include <vector>
 
@@ -5,12 +7,18 @@
 
 int main () {
     std::size_t size = 0;
-    std::cin >> size;
+    if (!scan_until_next_line(std::cin, size)) {
+        std::cerr << "failed to scan size\n";
+        return 1;
+    }
 
     mtx::Matrix<double> matrix(size);
     for (std::size_t i = 0; i < size; i++) {
         for (std::size_t j = 0; j < size; j++) {
-            std::cin >> matrix[i][j];
+            if (!scan_until_next_line(std::cin, matrix[i][j])) {
+                std::cerr << "failed to scan matrix[" << i << "][" << j << "]\n"; 
+                return 1;
+            }
         }
     }
 
